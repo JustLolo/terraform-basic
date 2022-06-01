8cf7c47692a7ffdfe0a0c20f00101f2eaa35364f
JustLolo <103621114+JustLolo@users.noreply.github.com>
2022-05-31 21:55:16 -0500

    some minor changes to providers.tf for clarification

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
     aws = {
       source = "hashicorp/aws"
       #version = "~> 3.0"
-      #in a production environment you have to fix version
+      #in a production environment you have to set fix version
     }
   }
 }
@@ -11,8 +11,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-west-2"
-  #shared_credentials_files = ["~/Arnaldo/.aws/credentials"]
-  #shared_config_files      = [file("~/.aws/config")]
+  #assuming you have configured aws cli already
+  shared_config_files      = [file("~/.aws/config")]
   shared_credentials_files = [file("~/.aws/credentials")]
 
   profile = "vscode"
