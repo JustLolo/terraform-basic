fd06a76cc57ae1a53b42750d90143c15b4f13e93
Arnaldo Ugarte Penton <arnoldupdev@gmail.com>
2022-06-06 00:13:11 -0500

    I fixed some issues I had, bc I will use env variables

--- providers.tf
+++ providers.tf
@@ -11,12 +11,16 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-west-2"
-  #assuming you have configured aws cli already
-  shared_config_files      = [file("~/.aws/config")]
-  shared_credentials_files = [file("~/.aws/credentials")]
+  #if you are using environment variables and have set aws already don't need to set the following configuration
 
-  
-  profile = "vscode"
+  #assuming you have configured aws cli already
+  #shared_config_files      = [file("~/.aws/config")]
+  #shared_credentials_files = [file("~/.aws/credentials")]
+
+
+
+
+  #profile = "vscode"
   #just set the actual profile you are using in aws cli
   #if you want terraform uses the same actual profile as aws cli
   #you should use a variable pointing to the env variable AWS_PROFILE
