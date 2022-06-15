e7267ca4c4dc445e6950a193dc207a0e7932686b
Arnaldo <augartep92@gmail.com>
2022-06-14 17:34:03 -0700

    modified:   .terraform.lock.hcl 	new file:   ansible/ansible.cfg 	new file:   ansible/ansibleConsultingPurposes.cfg 	new file:   ansible/inventory 	modified:   main.tf

--- main.tf
+++ main.tf
@@ -89,9 +89,18 @@ resource "aws_security_group" "mtc_sg" {
   }
 }
 
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "aws_key_pair" "mtc_auth" {
-  key_name   = "mtc_key"
-  public_key = file("~/.ssh/mtc_key.pub")
+  key_name   = "myKey"   # Create "myKey" to AWS
+  public_key = tls_private_key.pk.public_key_openssh
+
+  provisioner "local-exec" { # Create "myKey.pem" to your computer!!
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ~/.ssh/myKey.pem"
+  }
 }
 
 resource "aws_instance" "dev_node" {
