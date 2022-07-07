5fd45be08dbb6139d8bd5adf489ee5c52c58463a
Arnaldo Ugarte <arnoldupdev@gmail.com>
2022-07-07 15:40:48 -0700

    teraform script: the info section was changed to use an iterator

--- env-config/bin/terraform_info.sh
+++ env-config/bin/terraform_info.sh
@@ -11,29 +11,35 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 cd ../../
 
-declare -A command=( [0]="terraform apply -auto-approve"\
+declare -A command=( [0]="terraform state show"\
                      [1]="terraform state list | grep aws_instance"\
-                     [2]="terraform state show")
+                     [2]="terraform apply -auto-approve")
+                     
 
 if [ "$#" -lt 1 ]; then
     echo "----------------------------------"
     echo "0 parameters passed to this script"
     echo "----------------------------------"
-    echo "0 -> terraform apply -auto-approve"
-    echo "3 -> terraform state list | grep aws_instance"
-    echo "6 -> terraform state show #"
+    echo "REMEMBER: You can add a parameters o pipelines"
+    for key in "${!command[@]}"
+        do echo "$key - ${command[$key]}"
+    done
+
+
+    # echo "0 -> terraform apply -auto-approve"
+    # echo "3 -> terraform state list | grep aws_instance"
+    # echo "6 -> terraform state show #"
 fi
 
 if [ "$#" -eq 1 ]; then
     echo "----------------------------------"
     echo "1 parameters passed to this script"
     echo "----------------------------------"
-    
+
     # --> safe mode <--
     # command0="echo 'terraform apply -auto-approve'"
     # command3="echo 'terraform state list | grep aws_instance'"
     # command6="echo 'terraform state show #'"
-
-    read -p "Hit enter to run: ${command[$1]} " parameter
+    read -p "Enter to run -> ${command[$1]} " parameter
     eval ${command[$1]} $parameter
 fi
