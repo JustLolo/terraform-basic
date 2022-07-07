65bb5638b85531801f06f2d1b4e7177bb28ef87d
Arnaldo Ugarte <arnoldupdev@gmail.com>
2022-07-07 16:09:06 -0700

    added file and reading useful commands from file

--- env-config/bin/terraform_info.sh
+++ env-config/bin/terraform_info.sh
@@ -10,6 +10,21 @@ set -u
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 cd ../../
+file="./env-config/bin/frequently_used_terraform_commands"
+
+# TODO
+# fill the frequently_used_terraform_commands file where the first line will be the key/index
+# Finish this thing above, I have to save the read lines in the dictionary 'command'
+#                |
+#                |
+#                V
+# -n "$line" -> added due to the issue that doesn't show the last line
+# while IFS= read -r line || [ -n "$line" ]; do
+#     echo "$line"
+# done < "$file"
+
+
+
 
 declare -A command=( [0]="terraform state show"\
                      [1]="terraform state list | grep aws_instance"\
@@ -21,6 +36,8 @@ if [ "$#" -lt 1 ]; then
     echo "0 parameters passed to this script"
     echo "----------------------------------"
     echo "REMEMBER: You can add a parameters o pipelines"
+    file="./env-config/bin/frequently_used_terraform_commands"
+
     for key in "${!command[@]}"
         do echo "$key - ${command[$key]}"
     done
