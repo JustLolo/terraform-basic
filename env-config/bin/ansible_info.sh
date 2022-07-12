cff2010f0ec9a9d596af81f4cf6f3171a9ead3e7
Arnaldo Ugarte <arnoldupdev@gmail.com>
2022-07-12 08:19:36 -0700

    added git installation playbook and ansible useful commands script

---
+++ env-config/bin/ansible_info.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -i
+
+# We don't want the script writes in the history
+set +o history
+
+# checking parameters, modified, setting this will give me some problems checking 'e'
+# set -u
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+cd ../../ansible/
+file="../env-config/bin/commands_ansible"
+
+# try to load part of the environment using -> !/bin/bash -i 
+# this is nothing #!/usr/bin/env bash
+# --rcfile file
+# because I can chain commands
+
+# filling the dictionary
+declare -A command
+# -n "$line" -> added due to the issue that doesn't show the last line
+while IFS= read -r line || [ -n "$line" ]; do
+    commandKey="$(echo "$line" | cut -d " " -f 1)"
+    commandValue="$(echo "$line" | cut -d " " -f2-)"
+    command[$commandKey]=$commandValue
+done < "$file"
+
+if [ "$#" -lt 1 ]; then
+    echo "REMEMBER: -e for execution without user confirmation"
+    echo "REMEMBER: You can add parameters or pipelines"
+    echo ""
+    for key in "${!command[@]}"
+        do echo "$key - ${command[$key]}"
+    done
+fi
+
+if [ "$#" -eq 1 ]; then
+
+    read -p "Enter to run -> ${command[$1]} " parameter
+    eval ${command[$1]} $parameter
+fi
+
+if [[ "$#" -eq 2 ]]; then
+    if [[ "$2" -eq "-e" ]]; then
+        # "e" stands for execute without confirmation
+        # echo "${command[$1]}"
+        eval ${command[$1]}
+    fi
+fi
