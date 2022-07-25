117394eda42cede8c0d2a2a4aa28000f26ede7b6
Arnaldo Ugarte <arnoldupdev@gmail.com>
2022-07-25 13:13:34 -0700

    validation improve: - added validation of OS input - moved validation feature to a input_validation.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-locals {
-  # don't change this unless you want to add another kind of instance
-  instance_functionality_list = ["webapp", "database"]
-  instance_available_os       = ["ubuntu-20.04", "centos7"]
-}
-
 variable "instances" {
   type = map(object({ type = string, OS = string, amount = number }))
   default = {
@@ -15,17 +9,6 @@ variable "instances" {
   }
 }
 
-locals {
-  # validating instances input here, bc I can't validate a variable using another variable:
-  # terraform hasn't implemented this feature yet.
-  # open issue here: https://github.com/hashicorp/terraform/issues/25609
-  instance_var_ok = alltrue([
-    for instance in var.instances :
-    contains(local.instance_functionality_list, instance.type)
-  ]) ? true : file("--> make sure the kind of instance you want to create is one of these: ${join(", ", local.instance_functionality_list)}")
-  # using the function "file" bc terraform hasn't implemented a raise error feature yet
-  # so, this will return an error if it's false
-}
 
 variable "recreate_instances_after_apply" {
   type    = bool
